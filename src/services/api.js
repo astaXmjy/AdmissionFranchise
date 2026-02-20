8ca58f6923180839f38113a09ed6deb5e0d51e94
astaXmjy <yadavadarshgamer@gmail.com>
2026-02-20 22:03:36 +0530

    adding prod api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = 'https://admission-franchise-back.vercel.app';
-const API_BASE_URL ='http://127.0.0.1:8000';
+const API_BASE_URL = 'https://admission-franchise-back.vercel.app';
+// const API_BASE_URL ='http://127.0.0.1:8000';
 
 // Create axios instance
 const api = axios.create({
