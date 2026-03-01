b11f00f26cf72d12fd98e1b61ab4adc94bf219f5
astaXmjy <yadavadarshgamer@gmail.com>
2026-03-01 19:22:08 +0530

    changing base api to domain one

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'http://72.61.239.70';
+export const API_BASE_URL = 'https://skilledgelearning.cloud/api';
 // export const API_BASE_URL = 'http://127.0.0.1:8000';
 
 // Create axios instance
