73046800f3814fe557b9f3eafd27de018beab4f0
astaXmjy <yadavadarshgamer@gmail.com>
2026-03-03 16:48:05 +0530

    adding prod level backend api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// export const API_BASE_URL = 'https://skilledgelearning.cloud/api';
-export const API_BASE_URL = 'http://127.0.0.1:8000';
+export const API_BASE_URL = 'https://skilledgelearning.cloud/api';
+// export const API_BASE_URL = 'http://127.0.0.1:8000';
 
 // Create axios instance
 const api = axios.create({
